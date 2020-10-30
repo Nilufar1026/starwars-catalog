177e92b7512a6576868223670df0e9b01144d1aa
Nilufar1026 <71069308+Nilufar1026@users.noreply.github.com>
2020-10-30 14:00:45 +0100

    click2

--- js/main.js
+++ js/main.js
@@ -4,12 +4,28 @@ const cardDetailsText1 = document.querySelector(".text1")
 const cardDetailsTitle2 = document.querySelector(".title2")
 const cardDetailsText2 = document.querySelector(".text2")
 
-chooseList.addEventListener("click",function(){
+function showDetails(){
     cardDetailsTitle1.classList.add("visible") 
     cardDetailsText1.classList.add("visible") 
     cardDetailsTitle2.classList.add("visible") 
     cardDetailsText2.classList.add("visible") 
+}
+function hideDetails(){
+    cardDetailsTitle1.classList.remove("visible") 
+    cardDetailsText1.classList.remove("visible") 
+    cardDetailsTitle2.classList.remove("visible") 
+    cardDetailsText2.classList.remove("visible") 
+}
+// chooseList.addEventListener("click",showDetails)
+
+chooseList.addEventListener("click",function(event){
+    if(event.type=="click"){
+        showDetails()
+    }else{
+        hideDetails()
+    }
 })
+
 
 
 
